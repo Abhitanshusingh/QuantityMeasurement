1f41c7214d3b1a2bf4dd7f57b7a4f33acceee334
Abhitanshusingh <Abhitanshusingh99@gmail.com>
2020-04-09 23:33:24 +0530

    ADDED: Printing welcome message

---
+++ src/main/java/com/bridzelabz/service/QuantityMeasurementService.java
@@ -0,0 +1,7 @@
+package com.bridzelabz.service;
+
+public class QuantityMeasurementService {
+    public static void main(String[] args) {
+        System.out.println("Welcome to quality measurement service");
+    }
+}
