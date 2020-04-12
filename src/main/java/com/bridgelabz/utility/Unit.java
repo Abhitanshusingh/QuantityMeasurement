5ff0595f6607b318bd1651a36ecc0fa196e8bdf3
Abhitanshusingh <Abhitanshusingh99@gmail.com>
2020-04-12 17:35:39 +0530

    ADDED: Enum of Unit to specified the unit

---
+++ src/main/java/com/bridgelabz/utility/Unit.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.utility;
+
+public enum Unit {
+    FEET(12),INCH(1);
+
+    public double value;
+
+    Unit(double value) {
+        this.value = value;
+    }
+
+    public Double convertToBaseValue(Double length) {
+        return length * value;
+    }
+}
