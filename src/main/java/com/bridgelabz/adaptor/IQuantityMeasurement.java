6596d49645a77bff6c95293561d1de30f964e7a6
Abhitanshusingh <Abhitanshusingh99@gmail.com>
2020-04-12 20:10:11 +0530

    ADDED: addTwoNumber method

--- src/main/java/com/bridgelabz/adaptor/IQuantityMeasurement.java
+++ src/main/java/com/bridgelabz/adaptor/IQuantityMeasurement.java
@@ -5,4 +5,6 @@ import com.bridgelabz.utility.Length;
 public interface IQuantityMeasurement {
 
     public boolean compareValue(Length value1, Length value2);
+
+    public Double addTwoValue(Length value1, Length value2);
 }
