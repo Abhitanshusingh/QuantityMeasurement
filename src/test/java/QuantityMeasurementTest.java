007b877d12d1d3626440a42d475db7568cfa5248
Abhitanshusingh <Abhitanshusingh99@gmail.com>
2020-04-12 14:47:42 +0530

    ADDED: Test case to check two feet value are equal or not

---
+++ src/test/java/QuantityMeasurementTest.java
@@ -0,0 +1,32 @@
+import com.bridgelabz.utility.Length;
+import com.bridgelabz.utility.Unit;
+import com.bridgelabz.service.QuantityMeasurementService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class QuantityMeasurementTest {
+    QuantityMeasurementService quantityMeasurementService;
+    boolean result;
+
+    @Before
+    public void setUp() {
+        quantityMeasurementService = new QuantityMeasurementService();
+    }
+
+    @Test
+    public void givenZeroFeet_WhenEqualsZero_ShouldReturnTrue() {
+        Length firstValue = new Length(0.0, Unit.FEET);
+        Length secondValue = new Length(0.0, Unit.FEET);
+        result = quantityMeasurementService.compareValue(firstValue, secondValue);
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenNonZeroFeet_WhenNotEqualsZero_ShouldReturnFalse() {
+        Length firstValue = new Length(0.5, Unit.FEET);
+        Length secondValue = new Length(0.1, Unit.FEET);
+        result = quantityMeasurementService.compareValue(firstValue, secondValue);
+        Assert.assertEquals(false, result);
+    }
+}
