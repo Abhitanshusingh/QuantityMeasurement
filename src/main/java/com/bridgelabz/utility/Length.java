34285f753f3a04ee5553d133050417f7f5608b1f
Abhitanshusingh <Abhitanshusingh99@gmail.com>
2020-04-12 17:38:56 +0530

    ADDED: Overried equals method and assingn value in length method

---
+++ src/main/java/com/bridgelabz/utility/Length.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.utility;
+
+import java.util.Objects;
+
+public class Length {
+    Double length;
+
+    public  Length(Double distance, Unit unit) {
+        this.length = unit.convertToBaseValue(distance);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Length length1 = (Length) o;
+        return Objects.equals(length, length1.length);
+    }
+}
+
