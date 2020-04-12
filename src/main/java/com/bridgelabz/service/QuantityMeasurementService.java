ab17c13898723c6602a0848c0388d762541cad65
Abhitanshusingh <Abhitanshusingh99@gmail.com>
2020-04-12 14:44:28 +0530

    ADDED: compareValue method to compare object

--- src/main/java/com/bridgelabz/service/QuantityMeasurementService.java
+++ src/main/java/com/bridgelabz/service/QuantityMeasurementService.java
@@ -1,26 +1,14 @@
-package com.bridzelabz.service;
+package com.bridgelabz.service;
 
-import com.bridzelabz.conversions.Unit;
+import com.bridgelabz.adaptor.IQuantityMeasurement;
+import com.bridgelabz.utility.Length;
+import com.bridgelabz.adaptor.QuantityMeasurementFactory;
 
 public class QuantityMeasurementService {
-    private static double FIRST_VALUE;
-    private static double SECOND_VALUE;
 
-    //METHOD TO GET CONVERSIONS
-    public boolean getConversion(Unit type, double value1, double value2) {
-        setPreValues(type);
-        double operation1 = value1 * FIRST_VALUE;
-        double operation2 = value2 * SECOND_VALUE;
-
-        if (operation1 == operation2)
-            return true;                                                     BridgeLabz
-        if (operation1 != operation2)
-            return false;
-        return true;
-    }
-    //TO SET VALUES
-    private void setPreValues(Unit type) {
-        FIRST_VALUE = type.firstValue;
-        SECOND_VALUE = type.secondValue;
+    public boolean compareValue(Length value1, Length value2) {
+        IQuantityMeasurement object = QuantityMeasurementFactory.getObject();
+        boolean value = object.compareValue(value1, value2);
+        return value;
     }
 }
