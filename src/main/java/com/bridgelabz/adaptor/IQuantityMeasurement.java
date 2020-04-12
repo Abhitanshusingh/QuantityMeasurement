2f554c6694ba1d6a304d5089ab0a2994cdad6ada
Abhitanshusingh <Abhitanshusingh99@gmail.com>
2020-04-12 14:40:34 +0530

    ADDED: Interface

--- src/main/java/com/bridgelabz/adaptor/IQuantityMeasurement.java
+++ src/main/java/com/bridgelabz/adaptor/IQuantityMeasurement.java
@@ -4,5 +4,5 @@ import com.bridgelabz.utility.Length;
 
 public interface IQuantityMeasurement {
 
-    public boolean compareValue(Length valuu1, Length value2);
+    public boolean compareValue(Length value1, Length value2);
 }
