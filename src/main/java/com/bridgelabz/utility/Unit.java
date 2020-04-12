2efdb1e409af44e284f290bc2e649a21b631c21c
Abhitanshusingh <Abhitanshusingh99@gmail.com>
2020-04-12 23:38:19 +0530

    ADDED: FAHRENHEIT and CELSIUS as enum constan

--- src/main/java/com/bridgelabz/utility/Unit.java
+++ src/main/java/com/bridgelabz/utility/Unit.java
@@ -3,7 +3,8 @@ package com.bridgelabz.utility;
 public enum Unit {
     FEET(12.0),INCH(1.0),YARD(36.0),CENTIMETRE(0.4),
     GALLON(3.78), LITRES(1), MILLILITRES(0.001),
-    KILOGRAMS(1.0), GRAMS(0.001), TONNE(1000);;
+    KILOGRAMS(1.0), GRAMS(0.001), TONNE(1000),
+    FAHRENHEIT(1), CELSIUS(2.12);
 
     public Double value;
 
