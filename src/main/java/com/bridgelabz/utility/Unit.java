d372633a869080457d98c32b75cebb68c3a7cb5a
Abhitanshusingh <Abhitanshusingh99@gmail.com>
2020-04-12 21:22:37 +0530

    ADDED: MILLILITRE,LITRE,GALLON as enum constant

--- src/main/java/com/bridgelabz/utility/Unit.java
+++ src/main/java/com/bridgelabz/utility/Unit.java
@@ -1,7 +1,8 @@
 package com.bridgelabz.utility;
 
 public enum Unit {
-    FEET(12.0),INCH(1.0),YARD(36.0),CENTIMETER(0.4);
+    FEET(12.0),INCH(1.0),YARD(36.0),CENTIMETER(0.4),
+    GALLON(3780), LITRES(1000), MILLILITERS(1);
 
     public Double value;
 
