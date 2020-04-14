da520eff9909a8b83e2320a843503b5c8bfd4e77
Abhitanshusingh <Abhitanshusingh99@gmail.com>
2020-04-14 13:31:47 +0530

    REFACTORED: ADDED measurement type

--- src/main/java/com/bridgelabz/utility/Unit.java
+++ src/main/java/com/bridgelabz/utility/Unit.java
@@ -1,18 +1,38 @@
 package com.bridgelabz.utility;
 
 public enum Unit {
-    FEET(12.0),INCH(1.0),YARD(36.0),CENTIMETRE(0.4),
-    GALLON(3.78), LITRES(1), MILLILITRES(0.001),
-    KILOGRAMS(1.0), GRAMS(0.001), TONNE(1000),
-    FAHRENHEIT(1), CELSIUS(2.12);
+    FEET(12.0, MeasurementType.LENGHT),
+    INCH(1.0, MeasurementType.LENGHT),
+    YARD(36.0, MeasurementType.LENGHT),
+    CENTIMETRE(0.4, MeasurementType.LENGHT),
+
+    GALLON(3.78, MeasurementType.VOLUME),
+    LITRES(1, MeasurementType.VOLUME),
+    MILLILITRES(0.001, MeasurementType.VOLUME),
+
+    KILOGRAMS(1.0, MeasurementType.WEIGHT),
+    GRAMS(0.001, MeasurementType.WEIGHT),
+    TONNE(1000, MeasurementType.WEIGHT),
+
+    FAHRENHEIT(5.0 / 9.0, MeasurementType.TEMPERATURE),
+    CELSIUS(9.0 / 5.0, MeasurementType.TEMPERATURE);
 
     public Double value;
+    public MeasurementType QuantityType;
 
-    Unit(double value) {
+    Unit(double value, MeasurementType type) {
         this.value = value;
+        this.QuantityType = type;
     }
 
-    public Double convertToBaseValue(Double length) {
-        return length * value;
+    public Double convertToBaseValue(Double quantity, Unit unit) {
+        switch (unit) {
+            case FAHRENHEIT:
+                return ((quantity - 32) * (value));
+            case CELSIUS:
+                return ((quantity * (value)) + 32);
+            default:
+                return quantity * this.value;
+        }
     }
 }
