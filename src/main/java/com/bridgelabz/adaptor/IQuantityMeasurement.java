10b97c441c7554c16f6bbd4213076655f1a36d3b
Abhitanshusingh <Abhitanshusingh99@gmail.com>
2020-04-14 13:38:53 +0530

    Handled custom exception

--- src/main/java/com/bridgelabz/adaptor/IQuantityMeasurement.java
+++ src/main/java/com/bridgelabz/adaptor/IQuantityMeasurement.java
@@ -1,10 +1,11 @@
 package com.bridgelabz.adaptor;
 
+import com.bridgelabz.exception.QuantityMeasurementException;
 import com.bridgelabz.utility.Quantity;
 
 public interface IQuantityMeasurement {
 
-    public boolean compareValue(Quantity value1, Quantity value2);
+    public boolean compareValue(Quantity value1, Quantity value2) throws QuantityMeasurementException;
 
-    public Double addTwoValue(Quantity value1, Quantity value2);
+    public Double addTwoValue(Quantity value1, Quantity value2) throws QuantityMeasurementException;
 }
--- src/main/java/com/bridgelabz/service/QuantityMeasurementService.java
+++ src/main/java/com/bridgelabz/service/QuantityMeasurementService.java
@@ -1,18 +1,19 @@
 package com.bridgelabz.service;
 
 import com.bridgelabz.adaptor.IQuantityMeasurement;
+import com.bridgelabz.exception.QuantityMeasurementException;
 import com.bridgelabz.utility.Quantity;
 import com.bridgelabz.adaptor.QuantityMeasurementFactory;
 
 public class QuantityMeasurementService {
 
-    public boolean compareValue(Quantity value1, Quantity value2) {
+    public boolean compareValue(Quantity value1, Quantity value2) throws QuantityMeasurementException {
         IQuantityMeasurement object = QuantityMeasurementFactory.getObject();
         boolean result = object.compareValue(value1, value2);
         return result;
     }
 
-    public Double addTwoValues(Quantity value1 , Quantity value2) {
+    public Double addTwoValues(Quantity value1 , Quantity value2) throws QuantityMeasurementException {
         IQuantityMeasurement object = QuantityMeasurementFactory.getObject();
         Double result = object.addTwoValue(value1, value2);
         return result;
